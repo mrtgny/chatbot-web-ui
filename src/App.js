a900e1b8624a87d443dad281615840e6dbcf0948
Murat Güney <muratguney@Murat-MacBook-Pro.local>
2022-07-01 14:33:14 +0300

    server name is fixed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import './App.css';
-import {dateToStr} from "./utils";
-import sendIcon from "./images/send-icon.png";
 import coffeeCup from "./images/coffee-cup.png";
+import sendIcon from "./images/send-icon.png";
+import { dateToStr } from "./utils";
 
-const server = "coffeechatbot.cf/ws";
+const server = "coffeebot.mrtgny.com/ws";
 
 class App extends React.Component {
 
     constructor(props, context) {
         super(props, context);
-        this.state = {init: false};
+        this.state = { init: false };
     }
 
     componentDidMount() {
         setTimeout(() => {
-            this.setState({init: true})
+            this.setState({ init: true })
         }, 3000)
     }
 
 
     render() {
-        return this.state.init ? <ChatBot/> : <Welcome/>
+        return this.state.init ? <ChatBot /> : <Welcome />
     }
 }
 
@@ -29,8 +29,8 @@ const Welcome = () => {
     return (
         <div className="page center">
             <div className="container">
-                <div className="welcome-container" style={{height: "100%"}}>
-                    <div className="spread-column" style={{height: "100%"}}>
+                <div className="welcome-container" style={{ height: "100%" }}>
+                    <div className="spread-column" style={{ height: "100%" }}>
                         <div className="welcome-title">
                             Welcome to CoffeeInLove
                         </div>
@@ -47,11 +47,11 @@ const Welcome = () => {
 class ChatBot extends React.Component {
     constructor(props, context) {
         super(props, context);
-        this.connection = new WebSocket('ws://' + server);
+        this.connection = new WebSocket('wss://' + server);
 
         this.connection.onopen = (message) => {
             // this.connection is opened and ready to use
-            this.connection.send(JSON.stringify({message: "init-chatbot"}))
+            this.connection.send(JSON.stringify({ message: "init-chatbot" }))
 
         };
 
@@ -64,11 +64,11 @@ class ChatBot extends React.Component {
         return (
             <div className="page center">
                 <div className="container"
-                     style={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
-                    <div className="cell" style={{overflow: "auto"}} id="chat-container">
-                        <ChatContainer connection={this.connection}/>
+                    style={{ display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
+                    <div className="cell" style={{ overflow: "auto" }} id="chat-container">
+                        <ChatContainer connection={this.connection} />
                     </div>
-                    <TextField connection={this.connection}/>
+                    <TextField connection={this.connection} />
                 </div>
             </div>
         )
@@ -85,7 +85,7 @@ class ChatContainer extends React.Component {
 
 
     componentWillMount() {
-        const {connection} = this.props;
+        const { connection } = this.props;
         connection.onmessage = (message) => {
             let json;
             try {
@@ -108,10 +108,10 @@ class ChatContainer extends React.Component {
     }
 
     render() {
-        const {connection} = this.props;
+        const { connection } = this.props;
         return (
-            <div style={{width: "100%", height: "100%"}}>
-                <ChatContent messages={this.state.messages} connection={connection}/>
+            <div style={{ width: "100%", height: "100%" }}>
+                <ChatContent messages={this.state.messages} connection={connection} />
             </div>
         );
     }
@@ -119,25 +119,25 @@ class ChatContainer extends React.Component {
 }
 
 const ChatContent = (props) => {
-    const {messages, connection} = props;
+    const { messages, connection } = props;
     return messages.map((item, index) => {
-        const {name, message, list, options, status, coffees} = item;
+        const { name, message, list, options, status, coffees } = item;
         return [
-            name ? <Coffees key={"n"} name={name} coffees={coffees}/> : null,
-            status === 'typing' ? <Typing key={index + "t"} message={item}/> : null,
-            !!message ? <ChatItem key={index + "c"} message={item}/> : null,
-            (list || []).length ? <List key={index + "l"} message={item} connection={connection}/> : null,
-            (options || []).length ? <Options key={index + "o"} options={options} connection={connection}/> : null
+            name ? <Coffees key={"n"} name={name} coffees={coffees} /> : null,
+            status === 'typing' ? <Typing key={index + "t"} message={item} /> : null,
+            !!message ? <ChatItem key={index + "c"} message={item} /> : null,
+            (list || []).length ? <List key={index + "l"} message={item} connection={connection} /> : null,
+            (options || []).length ? <Options key={index + "o"} options={options} connection={connection} /> : null
         ].filter(i => i)
     })
 };
 
 const Coffees = props => {
-    const {name, coffees} = props;
+    const { name, coffees } = props;
     return coffees.map((coffee, index) => {
         return (
-            <div key={index} style={{display: 'inline-block'}}>
-                <Coffee name={name} size={coffee.size} coffee={coffee.coffee} count={coffee.count}/>
+            <div key={index} style={{ display: 'inline-block' }}>
+                <Coffee name={name} size={coffee.size} coffee={coffee.coffee} count={coffee.count} />
             </div>
         )
     })
@@ -146,7 +146,7 @@ const Coffees = props => {
 //2 tall filter coffee 3 grande cold brew 4 venti latte
 
 const Coffee = props => {
-    const {name, size, count, coffee} = props;
+    const { name, size, count, coffee } = props;
     const sizes = {
         tall: 100,
         grande: 150,
@@ -162,11 +162,11 @@ const Coffee = props => {
     return (
         <div className="coffee-cup-container">
             <div className="badge">
-                <div className="center" style={{height: "100%"}}>
+                <div className="center" style={{ height: "100%" }}>
                     x{count}
                 </div>
             </div>
-            <img src={coffeeCup} alt="Coffee Cup" className="coffee-cup-image" style={{width}}/>
+            <img src={coffeeCup} alt="Coffee Cup" className="coffee-cup-image" style={{ width }} />
             <div className="coffee-cup-name" style={nameStyle}>
                 {name}
             </div>
@@ -179,19 +179,19 @@ const Coffee = props => {
 
 const Typing = () => {
     return (
-        <div style={{display: "flex", justifyContent: "flex-start"}}>
+        <div style={{ display: "flex", justifyContent: "flex-start" }}>
             <div className="cell chatbot-chat-item"
-                 style={{position: "relative", width: 25, padding: "16px 16px 24px 16px"}}>
-                <span className="dot-bounce" style={{animationDelay: "0", left: 10}}>.</span>
-                <span className="dot-bounce" style={{animationDelay: "0.2s", left: 20}}>.</span>
-                <span className="dot-bounce" style={{animationDelay: "0.4s", left: 30}}>.</span>
+                style={{ position: "relative", width: 25, padding: "16px 16px 24px 16px" }}>
+                <span className="dot-bounce" style={{ animationDelay: "0", left: 10 }}>.</span>
+                <span className="dot-bounce" style={{ animationDelay: "0.2s", left: 20 }}>.</span>
+                <span className="dot-bounce" style={{ animationDelay: "0.4s", left: 30 }}>.</span>
             </div>
         </div>
     )
 }
 
 const Options = (props) => {
-    const {options, connection} = props;
+    const { options, connection } = props;
     return (
         <div className="spread">
             {options.map((option, index) => {
@@ -202,7 +202,7 @@ const Options = (props) => {
                             message: option,
                             date: new Date()
                         }))
-                    }}/>
+                    }} />
                 )
             })
             }
@@ -211,7 +211,7 @@ const Options = (props) => {
 };
 
 const Option = (props) => {
-    const {option, onClick} = props;
+    const { option, onClick } = props;
     return (
         <div className="big-cell option-item" onClick={onClick}>
             {option}
@@ -220,18 +220,18 @@ const Option = (props) => {
 };
 
 const List = (props) => {
-    const {message: {list}, connection} = props;
+    const { message: { list }, connection } = props;
     return (
         <div className="list center">
-            <div className="cell" style={{width: '100%'}}>
-                <ul style={{listStyle: "none", margin: 0, padding: 0, width: "100%"}}>
+            <div className="cell" style={{ width: '100%' }}>
+                <ul style={{ listStyle: "none", margin: 0, padding: 0, width: "100%" }}>
                     {list.map((listItem, index) => {
                         return <li key={index} className="cell list-item"
-                                   onClick={() => connection.send(JSON.stringify({
-                                       author: "user",
-                                       message: listItem,
-                                       date: new Date()
-                                   }))}>{listItem}</li>
+                            onClick={() => connection.send(JSON.stringify({
+                                author: "user",
+                                message: listItem,
+                                date: new Date()
+                            }))}>{listItem}</li>
                     })}
                 </ul>
             </div>
@@ -240,21 +240,21 @@ const List = (props) => {
 };
 
 const ChatItem = (props) => {
-    const {message: {author, message, date}} = props;
+    const { message: { author, message, date } } = props;
     const className = author === "chatbot" ? "chatbot-chat-item" : "user-chat-item";
     const justifyContent = author === "chatbot" ? "flex-start" : "flex-end";
     return (
-        <div style={{display: "flex", justifyContent}}>
+        <div style={{ display: "flex", justifyContent }}>
             <div className={"cell " + className}>
                 <p>{message}</p>
-                <DateText date={date}/>
+                <DateText date={date} />
             </div>
         </div>
     )
 }
 
 const DateText = props => {
-    const {date} = props;
+    const { date } = props;
     return (
         <span className="date-text">{dateToStr(date)}</span>
     )
@@ -272,8 +272,8 @@ class TextField extends React.Component {
 
     onSend() {
 
-        const {connection} = this.props;
-        const {message} = this.state;
+        const { connection } = this.props;
+        const { message } = this.state;
         if (!message) return;
         const request = {
             author: "user",
@@ -281,13 +281,13 @@ class TextField extends React.Component {
             date: new Date()
         };
         connection.send(JSON.stringify(request));
-        this.setState({message: ""});
+        this.setState({ message: "" });
 
     }
 
     onChange(value) {
         const message = value;
-        const {connection} = this.props;
+        const { connection } = this.props;
         const request = {
             author: "user",
             message,
@@ -295,37 +295,37 @@ class TextField extends React.Component {
             date: new Date()
         };
         connection.send(JSON.stringify(request));
-        this.setState({message})
+        this.setState({ message })
     }
 
     render() {
         return (
-            <div className={'center input-container'} style={{position: "relative"}}>
+            <div className={'center input-container'} style={{ position: "relative" }}>
                 <input type="text"
-                       id={"text-field"}
-                       onChange={e => this.onChange(e.target.value)}
-                       value={this.state.message}
-                       className="full-width text-field"
-                       onKeyPress={e => e.key === "Enter" ? this.onSend() : null}
+                    id={"text-field"}
+                    onChange={e => this.onChange(e.target.value)}
+                    value={this.state.message}
+                    className="full-width text-field"
+                    onKeyPress={e => e.key === "Enter" ? this.onSend() : null}
                 />
                 <Suggestions connection={this.props.connection}
-                             message={this.state.message}
-                             onChange={this.onChange}
+                    message={this.state.message}
+                    onChange={this.onChange}
                 />
-                <SendButton disabled={!this.state.message} onSend={this.onSend}/>
+                <SendButton disabled={!this.state.message} onSend={this.onSend} />
             </div>
         )
     }
 }
 
 const SendButton = props => {
-    const {onSend, disabled} = props;
+    const { onSend, disabled } = props;
     const className = disabled ? "send-button-disabled" : "send-button";
     return (
-        <div style={{margin: 8}}>
+        <div style={{ margin: 8 }}>
             <div className={"center " + className}
-                 onClick={onSend}>
-                <img src={sendIcon} alt="Send" width="20" height="20"/>
+                onClick={onSend}>
+                <img src={sendIcon} alt="Send" width="20" height="20" />
             </div>
         </div>
     )
@@ -336,12 +336,12 @@ class Suggestions extends React.Component {
 
     constructor(props, context) {
         super(props, context);
-        this.state = {suggestions: []}
+        this.state = { suggestions: [] }
         this.onSelect = this.onSelect.bind(this);
     }
 
     onSelect(suggest) {
-        const {onChange} = this.props
+        const { onChange } = this.props
         const textField = document.getElementById("text-field");
         const value = textField.value;
         if (suggest.indexOf(value) === 0) {
@@ -357,7 +357,7 @@ class Suggestions extends React.Component {
 
 
     componentWillMount() {
-        const {connection} = this.props;
+        const { connection } = this.props;
         const old = connection.onmessage
         connection.onmessage = (message) => {
             let json;
@@ -369,7 +369,7 @@ class Suggestions extends React.Component {
             }
 
             if (json && json.suggest) {
-                this.setState({suggestions: json.message})
+                this.setState({ suggestions: json.message })
             } else {
                 old(message)
             }
@@ -378,19 +378,19 @@ class Suggestions extends React.Component {
 
 
     render() {
-        const {message} = this.props;
+        const { message } = this.props;
         if (!message)
             return null;
         return (
-            <div style={{position: "absolute", left: 8, bottom: 64, backgroundColor: "#eee"}}>
+            <div style={{ position: "absolute", left: 8, bottom: 64, backgroundColor: "#eee" }}>
                 {(this.state.suggestions || []).map((suggest, index) => {
                     if (index < 5) {
                         return (
                             <div key={index}
-                                 className="cell clickable"
-                                 tabIndex={0}
-                                 onClick={() => this.onSelect(suggest)}
-                                 onKeyPress={e => e.key === "Enter" && this.onSelect(suggest)}
+                                className="cell clickable"
+                                tabIndex={0}
+                                onClick={() => this.onSelect(suggest)}
+                                onKeyPress={e => e.key === "Enter" && this.onSelect(suggest)}
                             >
                                 {suggest}
                             </div>
