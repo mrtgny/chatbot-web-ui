3f798b04ce4bcc088fd99968d91953b5e5d64eca
Murat Güney <muratguney@Murat-MacBook-Pro.local>
2022-09-10 01:44:54 +0300

    overflow is fixed

--- src/components/ChatBot/index.tsx
+++ src/components/ChatBot/index.tsx
@@ -18,7 +18,7 @@ const ChatBot = () => {
 
     return (
         <div className="page flex justify-center items-center">
-            <div className="container flex flex-col translate-x-0" style={{ height: (height as number) - 86 }}>
+            <div className="container flex flex-col translate-x-0" style={{ height: (height as number) }}>
                 <div className="m-2 p-2 overflow-auto" id="chat-container" style={{ height: 'calc(100% - 56px)' }}>
                     <ChatContainer />
                 </div>
